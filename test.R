430b54887dd45b54aa1d461c00456d94cbc54559
easyhee <jihee001004@gmail.com>
2023-11-29 18:47:28 +0900

    1인여성가구수 전처리 수정함. 데이터 파일 구글드라이브에서 다시 다운 받아서 bdata/raw에 저장하고 실행해야함!

--- test.R
+++ test.R
@@ -6,15 +6,16 @@ library(readr)
 
 dir()
 
-data <- read.csv("1인가구(성별_연령별)_2015-2022.csv", fileEncoding = "UTF-8") %>% as_tibble()
+data <- read.csv("1인여성가구_2015-2022.csv", fileEncoding = "UTF-8") %>% as_tibble()
 crime <- read.csv("서울시_5대범죄발생현황_2015-2022.csv")
+
+colnames(data)
 
 View(data)
 data_rename <- data %>%
-  rename(GU_NM = '자치구별.2.',
-         GENDER = 성별,
-         TOTAL_AGE = 연령별_합계,
-         BASE_YEAR = 기준년도)
+  rename(GU_NM = 자치구명,
+         GENDER = 여자,
+         BASE_YEAR = 시점)
 
 crime_rename <-crime %>%
   rename(GU_NM =자치구명,
@@ -23,5 +24,5 @@ crime_rename <-crime %>%
          SEXUAL_ASS = 강간_강제추행,
          THEFT = 절도,
          VIOLENCE = 폭력,
-         TOTAL_CRIME = 5대범죄_소계,
+         TOTAL_CRIME = 5대범죄소계,
          BASE_YEAR = 기준년도)
