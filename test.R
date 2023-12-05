63573fb5193a15d2591e390839b558786bd98914
easyhee <jihee001004@gmail.com>
2023-12-05 23:14:34 +0900

    삭제함

--- test.R
+++ test.R
@@ -7,22 +7,26 @@ library(readr)
 dir()
 
 data <- read.csv("1인여성가구_2015-2022.csv", fileEncoding = "UTF-8") %>% as_tibble()
-crime <- read.csv("서울시_5대범죄발생현황_2015-2022.csv")
+crime <- read.csv("서울시_5대범죄발생현황_2015-2022.csv", fileEncoding = "UTF-8") %>% as_tibble()
 
 colnames(data)
-
-View(data)
+#View(data)
 data_rename <- data %>%
   rename(GU_NM = 자치구명,
          GENDER = 여자,
          BASE_YEAR = 시점)
 
+colnames(crime)
+View(crime)
 crime_rename <-crime %>%
   rename(GU_NM =자치구명,
+         BASE_YEAR = 시점,
+         TOTAL_CRIME = 소계,
          HOMICIDE = 살인,
          ROBBERY = 강도,
          SEXUAL_ASS = 강간_강제추행,
          THEFT = 절도,
-         VIOLENCE = 폭력,
-         TOTAL_CRIME = 5대범죄소계,
-         BASE_YEAR = 기준년도)
+         VIOLENCE = 폭력) %>%
+  select(GU_NM = "자치구명")
+
+
