85b93d3707bac6c3a72c4a64ee223bffa71f147c
easyhee <jihee001004@gmail.com>
2023-12-08 01:34:15 +0900

    전처리 거의 최종

--- preprocess.R
+++ preprocess.R
@@ -3,30 +3,59 @@ setwd("c:/bdata/raw")
 library(tidyverse)
 library(readr)
 
-
 dir()
 
-data <- read.csv("1인여성가구_2015-2022.csv", fileEncoding = "UTF-8") %>% as_tibble()
-crime <- read.csv("서울시_5대범죄발생현황_2015-2022.csv", fileEncoding = "UTF-8") %>% as_tibble()
+#1인 여성 가구수 데이터 불러오기
+data <- read.csv("1인여성가구_2015-2022.csv",stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
 
-colnames(data)
-#View(data)
-data_rename <- data %>%
-  rename(GU_NM = 자치구명,
-         GENDER = 여자,
-         BASE_YEAR = 시점)
+#자치구명과 여성 가구수만 추출
+data <- data[, c("시점", "자치구명", "여자")]
+#행 이름 수정
+rownames(data) <- NULL
+#시점 컬럼명 수정
+colnames(data)[1] <- "연도"
+#데이터 형식 통일 : 숫자로
+data$여자 <- as.integer(data$여자)
 
-colnames(crime)
-View(crime)
-crime_rename <-crime %>%
-  rename(GU_NM =자치구명,
-         BASE_YEAR = 시점,
-         TOTAL_CRIME = 소계,
-         HOMICIDE = 살인,
-         ROBBERY = 강도,
-         SEXUAL_ASS = 강간_강제추행,
-         THEFT = 절도,
-         VIOLENCE = 폭력) %>%
-  select(GU_NM = "자치구명")
+crime_2015 <- read.csv("5대범죄발생현황_2015.csv", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
+crime_2016 <- read.csv("5대범죄발생현황_2016.csv", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
+crime_2017 <- read.csv("5대범죄발생현황_2017.csv", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
+crime_2018 <- read.csv("5대범죄발생현황_2018.csv", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
+crime_2019 <- read.csv("5대범죄발생현황_2019.csv", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
+crime_2020 <- read.csv("5대범죄발생현황_2020.csv", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
+crime_2021 <- read.csv("5대범죄발생현황_2021.csv", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
+crime_2022 <- read.csv("5대범죄발생현황_2022.csv", stringsAsFactors = FALSE, header = TRUE, fileEncoding = "UTF-8") %>% as_tibble()
 
+
+crime_2015 <- crime_2015 %>% mutate(살인 =  as.integer(살인))
+crime_2016 <- crime_2016 %>% mutate(살인 =  as.integer(살인))
+crime_2017 <- crime_2017 %>% mutate(살인 =  as.integer(살인))
+crime_2018 <- crime_2018 %>% mutate(살인 =  as.integer(살인))
+crime_2019 <- crime_2019 %>% mutate(살인 =  as.integer(살인))
+crime_2020 <- crime_2020 %>% mutate(살인 =  as.integer(살인))
+crime_2021 <- crime_2021 %>% mutate(살인 =  as.integer(살인))
+crime_2022 <- crime_2022 %>% mutate(살인 =  as.integer(살인))
+
+crime_2015 <- crime_2015 %>% mutate(강도 =  as.integer(강도))
+crime_2016 <- crime_2016 %>% mutate(강도 =  as.integer(강도))
+crime_2017 <- crime_2017 %>% mutate(강도 =  as.integer(강도))
+crime_2018 <- crime_2018 %>% mutate(강도 =  as.integer(강도))
+crime_2019 <- crime_2019 %>% mutate(강도 =  as.integer(강도))
+crime_2020 <- crime_2020 %>% mutate(강도 =  as.integer(강도))
+crime_2021 <- crime_2021 %>% mutate(강도 =  as.integer(강도))
+crime_2022 <- crime_2022 %>% mutate(강도 =  as.integer(강도))
+
+crime_dt <- bind_rows(crime_2015,crime_2016,crime_2017,crime_2018,
+                      crime_2019,crime_2020,crime_2021,crime_2022)
+
+crime_dt <- crime_dt %>% 
+  mutate(강도 =  as.integer(강도))
+
+crime_dt <- crime_dt %>%
+  select(자치구명, 연도, 살인, 강도, 강간, 절도, 폭력) 
+
+final_dt <- data %>% 
+  left_join(crime_dt, by = c("자치구명", "연도"))
+
+write.csv(final_dt, "preprocessed_data.csv", row.names = FALSE)
 
